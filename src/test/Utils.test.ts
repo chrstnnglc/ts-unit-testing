a2e549b8d2b6c3458fae0bc9223eb3dd7e0da48c
chrstnnglc <catfelizardo@gmail.com>
2021-11-02 09:43:21 +0800

    Added code and files from project setup (Lesson 6)

---
+++ src/test/Utils.test.ts
@@ -0,0 +1,9 @@
+import { Utils } from '../app/Utils';
+
+
+describe('Utils test suite', () => {
+    it('should return a message on the console', () => {
+        const msg = Utils.toUpperCase('ssw');
+        console.log('Itsa me a test')
+    })
+});
