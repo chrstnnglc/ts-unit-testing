702e758362a4a5d6963cd8176fc7b072b5ab3b8d
chrstnnglc <catfelizardo@gmail.com>
2021-11-15 11:38:14 +0800

    Added code from Lesson 7

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -4,6 +4,24 @@ import { Utils } from '../app/Utils';
 describe('Utils test suite', () => {
     it('should return a message on the console', () => {
         const msg = Utils.toUpperCase('ssw');
-        console.log('Itsa me a test')
+        expect(msg).toBe('SSW');
+    })
+
+    it('should parse simple URL', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login');
+        expect(parsedUrl.href).toBe('http://localhost:8080/login');
+        expect(parsedUrl.port).toBe('8080');
+        expect(parsedUrl.protocol).toBe('http:');
+        expect(parsedUrl.query).toEqual({});
+    })
+
+    it('should parse URL with query', () => {
+        const parsedUrl = Utils.parseUrl('http://localhost:8080/login?user=user&password=pass');
+        const expectedQuery = {
+            user: 'user',
+            password: 'pass'
+        }
+        expect(parsedUrl.query).toEqual(expectedQuery);
+        expect(expectedQuery).toBe(expectedQuery);
     })
 });
